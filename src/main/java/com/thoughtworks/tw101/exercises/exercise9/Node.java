db68592982539b6e6052af396fe592bbca0340f5
kthong3 <kthongsakounh@gmail.com>
2018-07-09 19:06:36 -0500

    print names in alphabetical order by first letter

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,42 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
+    private List<String> names;
 
-    public Node(String name) {
+    public Node(String name, List<String> names) {
         this.name = name;
+        this.names = names;
     }
 
     public void add(String nameOfNewNode) {
+        if(names.isEmpty()){
+            names.add(nameOfNewNode);
+        }
+        else {
+            names.add(nameOfNewNode);
+            names = organize(names);
+        }
     }
 
     public List<String> names() {
-        return null;
+        return organize(names);
+    }
+
+    private List<String> organize(List<String> names){
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        List<String> ordered = new ArrayList<>();
+        for (int i = 0; i < alphabet.length(); i++) {
+            for (int j = 0; j < names.size(); j++) {
+                if (names.get(j).charAt(0) == alphabet.charAt(i)){
+                    ordered.add(names.get(j));
+                }
+            }
+        }
+        return ordered;
     }
 }
