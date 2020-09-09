99dcadaaf88b76988c1d89d1de817758405757ef
kthong3 <kthongsakounh@gmail.com>
2020-09-09 09:57:45 -0500

    use for loop to increment and then print total

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    private Integer count;
+
+    public Accumulator() {
+        this.count = 0;
+    }
+
+    public void total() {
+        System.out.println(this.count);
+    }
+
+    public Integer increment() {
+        return this.count++;
+    }
 }
